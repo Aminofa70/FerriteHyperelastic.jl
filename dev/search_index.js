e0b167d29f8d8ae10d539196d81ef4bf8dac243c
Documenter.jl <documenter@juliadocs.github.io>
2025-07-28 09:24:03 +0000

    build based on dcdb5f5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FerriteHyperelastic","page":"Home","title":"FerriteHyperelastic","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FerriteHyperelastic.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
